70f12ba911c6576df8d52a5c27c673f9944033e7
Luis Azocar <>
2022-07-11 22:50:30 -0300

    test transform css

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
 import "./style.css";
 
-document.querySelector("#app1").innerHTML = `
-<section class="container">
-  <figure class="item-one">
-    <div class="image image-one"></div>
-    <figcaption class="title title-one">
-      <p>Retail</p>
-    </figcaption>
-  </figure>
-  <figure class="item-two">
-    <div class="image image-two"></div>
-    <figcaption class="title title-two">
-      <p>Direct-To-Consume(DTO)</p>
-    </figcaption>
-  </figure>
-</section>
-`;
+// document.querySelector("#app1").innerHTML = `
+// <section class="AppOne__container">
+//   <figure class="AppOne__item-one">
+//     <div class="AppOne__image AppOne__image-one"></div>
+//     <figcaption class="AppOne__title AppOne__title-one">
+//       <p>Retail</p>
+//     </figcaption>
+//   </figure>
+//   <figure class="AppOne__item-two">
+//     <div class="AppOne__image AppOne__image-two"></div>
+//     <figcaption class="AppOne__title AppOne__title-two">
+//       <p>Direct-To-Consume(DTO)</p>
+//     </figcaption>
+//   </figure>
+// </section>
+// `;
 
 document.querySelector("#app2").innerHTML = `
-<section class="container">
-    <div class="item"></div>
-    <div class="item"></div>
+<section class="AppTwo__container">
+    <div class="AppTwo__item AppTwo__image-one"></div>
+    <div class="AppTwo__item AppTwo__image-two"></div>
 </section>
 `;
 
-document.querySelector("#app3").innerHTML = `
-<section class="container">
-    <div class="item"></div>
-    <div class="item"></div>
-</section>
-`;
+// document.querySelector("#app3").innerHTML = `
+// <section class="container">
+//     <div class="item"></div>
+//     <div class="item"></div>
+// </section>
+// `;
