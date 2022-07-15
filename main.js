e97ca292c27f53b4bcacf2422fc398ae3ce39372
Luis Azocar <>
2022-07-15 09:09:36 -0300

    second solution using transforms

--- main.js
+++ main.js
@@ -1,32 +1,31 @@
 import "./style.css";
 
-// document.querySelector("#app1").innerHTML = `
-// <section class="AppOne__container">
-//   <figure class="AppOne__item-one">
-//     <div class="AppOne__image AppOne__image-one"></div>
-//     <figcaption class="AppOne__title AppOne__title-one">
-//       <p>Retail</p>
-//     </figcaption>
-//   </figure>
-//   <figure class="AppOne__item-two">
-//     <div class="AppOne__image AppOne__image-two"></div>
-//     <figcaption class="AppOne__title AppOne__title-two">
-//       <p>Direct-To-Consume(DTO)</p>
-//     </figcaption>
-//   </figure>
-// </section>
-// `;
-
-document.querySelector("#app2").innerHTML = `
-<section class="AppTwo__container">
-    <div class="AppTwo__item AppTwo__image-one"></div>
-    <div class="AppTwo__item AppTwo__image-two"></div>
+document.querySelector("#app1").innerHTML = `
+<section class="AppOne__container">
+  <figure class="AppOne__item-one">
+    <div class="AppOne__image AppOne__image-one"></div>
+    <figcaption class="AppOne__title AppOne__title-one">
+      <p>Retail</p>
+    </figcaption>
+  </figure>
+  <figure class="AppOne__item-two">
+    <div class="AppOne__image AppOne__image-two"></div>
+    <figcaption class="AppOne__title AppOne__title-two">
+      <p>Direct-To-Consume(DTO)</p>
+    </figcaption>
+  </figure>
 </section>
 `;
 
-// document.querySelector("#app3").innerHTML = `
-// <section class="container">
-//     <div class="item"></div>
-//     <div class="item"></div>
+// document.querySelector("#app2").innerHTML = `
+// <section class="AppTwo__container">
+//     <div class="AppTwo__item AppTwo__image-one">
+//     <span>Retail</span>
+//     </div>
+//     <div class="container_two">
+//         <div class="AppTwo__item AppTwo__image-two">
+//         <span>Direct-To-Consume(DTO)</span>
+//         </div>
+//     </div>
 // </section>
 // `;
